defc35ecbebc49efe149e1cade5166650901d98f
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-11-09 12:54:04 +0600

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { pinoConfig } from './common/config/pino.config';
+import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
@@ -13,6 +14,7 @@ import { UserModule } from './modules/user/user.module';
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
