e90f9797beb2694f5494935644a14a48f358c3ea
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-11-24 00:19:16 +0600

    customized sidebar

--- src/views/resumes/editor/config.tsx
+++ src/views/resumes/editor/config.tsx
@@ -8,17 +8,16 @@ import {
   LucideStar,
   LucideUser2,
 } from "lucide-react";
-import { ReactNode } from "react";
 
-export type NavItem = { icon: ReactNode; id: number; label: string };
+export type NavItem = { icon: React.ElementType; id: number; label: string };
 
 export const navs: NavItem[] = [
-  { id: 0, label: "Personal Info", icon: <LucideUser2 /> },
-  { id: 1, label: "Contacts", icon: <LucideContact2 /> },
-  { id: 2, label: "Educations", icon: <LucideGraduationCap /> },
-  { id: 3, label: "Experiences", icon: <LucideBriefcase /> },
-  { id: 4, label: "Projects", icon: <LucideStar /> },
-  { id: 5, label: "Technical Skills", icon: <LucidePencilRuler /> },
-  { id: 6, label: "Language Skills", icon: <LucideLanguages /> },
-  { id: 7, label: "Socials", icon: <LucideLink2 /> },
+  { id: 0, label: "Personal Info", icon: LucideUser2 },
+  { id: 1, label: "Contacts", icon: LucideContact2 },
+  { id: 2, label: "Educations", icon: LucideGraduationCap },
+  { id: 3, label: "Experiences", icon: LucideBriefcase },
+  { id: 4, label: "Projects", icon: LucideStar },
+  { id: 5, label: "Technical Skills", icon: LucidePencilRuler },
+  { id: 6, label: "Language Skills", icon: LucideLanguages },
+  { id: 7, label: "Socials", icon: LucideLink2 },
 ];
--- src/views/resumes/editor/sections/sidebar/item.tsx
+++ src/views/resumes/editor/sections/sidebar/item.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { setNav } from "@/store/features/editor/slice";
 import { useAppDispatch, useEditor } from "@/store/hooks";
 import { NavItem } from "../../config";
@@ -11,14 +17,26 @@ const Item = (props: Props) => {
   const { data } = props;
   const { nav } = useEditor();
   const dispatch = useAppDispatch();
+
+  const Icon = data.icon;
+
   return (
-    <Button
-      variant={nav === data.id ? "secondary" : "ghost"}
-      size={"icon"}
-      onClick={() => dispatch(setNav(data.id))}
-    >
-      {data.icon}
-    </Button>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant={nav === data.id ? "secondary" : "ghost"}
+            size={"icon"}
+            onClick={() => dispatch(setNav(data.id))}
+          >
+            <Icon size={16} />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          <p>{data.label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
