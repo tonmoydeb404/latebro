13b6f6478f5b27e55d68fa6d7852c98f712d0fde
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-10 01:28:59 +0600

    added new frontend domain support

--- src/common/utills/request.ts
+++ src/common/utills/request.ts
@@ -1,15 +1,9 @@
 import { Request } from 'express';
 
 export const getDomain = (req: Request) => {
-  const host = req.headers.host;
+  const origin = req.headers.origin;
 
-  const [domain, _] = host.split(':');
-  // Check if the host is a subdomain (e.g., api.example.com)
-  const parts = domain.split('.');
-  if (parts.length > 2) {
-    // Return the top-level domain (e.g., example.com)
-    return parts.slice(-2).join('.');
-  }
-  // Return the host as-is for single-level domains
-  return domain;
+  if (!origin) return undefined;
+
+  return new URL(origin).hostname;
 };
