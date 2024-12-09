069e536009661cfb8c1935d96b161c1be339eaf5
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-09 23:16:58 +0600

    added back to templates icon

--- src/views/resumes/editor/sections/sidebar/index.tsx
+++ src/views/resumes/editor/sections/sidebar/index.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { paths } from "@/router/paths";
+import { LucideHome } from "lucide-react";
+import Link from "next/link";
 import { navs } from "../../config";
 import Item from "./item";
 import ResumeDropdown from "./resume-dropdown";
@@ -9,6 +13,16 @@ const Sidebar = (props: Props) => {
   return (
     <ScrollArea className="border-r shrink-0">
       <div className="flex flex-col gap-1 px-2 py-5">
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          className="max-sm:size-9 mb-1.5"
+          asChild
+        >
+          <Link href={paths.resumes.root}>
+            <LucideHome size={18} />
+          </Link>
+        </Button>
         <ResumeDropdown />
         {navs.map((item) => (
           <Item key={item.id} data={item} />
