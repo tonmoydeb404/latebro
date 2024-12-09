288f8f539f06a47bfb1076d0e66e806bc2484d25
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-10 02:17:44 +0600

    set http only false

--- src/modules/auth/config/cookie.config.ts
+++ src/modules/auth/config/cookie.config.ts
@@ -3,7 +3,7 @@ import { CookieOptions, Request } from 'express';
 
 export const getCookieConfig = (req: Request): CookieOptions => {
   return {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === 'production',
     signed: true,
     maxAge: 24 * 60 * 60 * 1000,
