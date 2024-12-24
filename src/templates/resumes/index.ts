2f6238751fa2f5b6212e8b1a9c5798c194a799ef
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-24 16:14:16 +0600

    disabled some template for now

--- src/templates/resumes/index.ts
+++ src/templates/resumes/index.ts
@@ -24,42 +24,42 @@ const templates: Template[] = [
     text: "",
     import: () => import("@/templates/resumes/template-3"),
   },
-  {
-    id: "4",
-    title: "Template 4",
-    text: "",
-    import: () => import("@/templates/resumes/template-4"),
-  },
-  {
-    id: "5",
-    title: "Template 5",
-    text: "",
-    import: () => import("@/templates/resumes/template-5"),
-  },
-  {
-    id: "6",
-    title: "Template 6",
-    text: "",
-    import: () => import("@/templates/resumes/template-6"),
-  },
-  {
-    id: "7",
-    title: "Template 7",
-    text: "",
-    import: () => import("@/templates/resumes/template-7"),
-  },
-  {
-    id: "8",
-    title: "Template 8",
-    text: "",
-    import: () => import("@/templates/resumes/template-8"),
-  },
-  {
-    id: "9",
-    title: "Template 9",
-    text: "",
-    import: () => import("@/templates/resumes/template-9"),
-  },
+  // {
+  //   id: "4",
+  //   title: "Template 4",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-4"),
+  // },
+  // {
+  //   id: "5",
+  //   title: "Template 5",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-5"),
+  // },
+  // {
+  //   id: "6",
+  //   title: "Template 6",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-6"),
+  // },
+  // {
+  //   id: "7",
+  //   title: "Template 7",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-7"),
+  // },
+  // {
+  //   id: "8",
+  //   title: "Template 8",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-8"),
+  // },
+  // {
+  //   id: "9",
+  //   title: "Template 9",
+  //   text: "",
+  //   import: () => import("@/templates/resumes/template-9"),
+  // },
 ];
 
 export const getTemplate = (id: string) => templates.find((t) => t.id === id);
